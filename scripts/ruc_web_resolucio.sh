e88fbd3156891eb094a980c9224175c5aab21e50
oscar <oprietop@uoc.edu>
2009-01-29 11:31:17 +0100

    -files

--- scripts/ruc_web_resolucio.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. tren.env
-
-#firefox "http://cv.uoc.edu/tren/trenacc?s=$(sessio_tren)&modul=RUC.RESOLUCION_PETICION/buscarPeticions.do&subaccio=iniciar&entidad_gestora=GOT"
-firefox "http://cv.uoc.edu/tren/trenacc?s=$(sessio_tren)&modul=RUC.RESOLUCION_PETICION/buscarPeticions.do&subaccio=iniciar&entidad_gestora=GOT&assignadasSelec=Peticions assignades"
