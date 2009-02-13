4519a1ad17036a2cb64f4a0e88cb48f9f55f5fdd
oscar <oprietop@uoc.edu>
2009-02-13 15:04:37 +0100

    añado script test.pl

--- scripts/test.pl
+++ scripts/test.pl
@@ -1,8 +1,7 @@
-#!/usr/bin/perl -wT
+#!/usr/bin/perl -w
 # http://search.cpan.org/src/HARDAKER/SNMP-5.0401/t/bulkwalk.t
 
-
-print "Content-Type: text/html\n\n";
+#print "Content-Type: text/html\n\n";
 
 use strict;
 use SNMP;
@@ -11,25 +10,33 @@ use Term::ANSIColor qw(:constants);
 $Term::ANSIColor::AUTORESET = 1;
 
 my %oids =(
-    vmMembershipEntry           =>  '.1.3.6.1.4.1.9.9.68.1.2.2.1.2',    #CISCO
-    vtpVlanName                 =>  '.1.3.6.1.4.1.9.9.46.1.3.1.1.4.1',  #CISCO
-    dot1dBasePortIfIndex        =>  '.1.3.6.1.2.1.17.1.4.1.2',          #ENT
-    dot1qVlanStaticName         =>  '.1.3.6.1.2.1.17.7.1.4.3.1.1',      #ENT
-    dot1qPvid                   =>  '.1.3.6.1.2.1.17.7.1.4.5.1.1',      #ENT
-    ifIndex                     =>  '.1.3.6.1.2.1.2.2.1.1',
-    ifName                      =>  '.1.3.6.1.2.1.31.1.1.1.1',
-    ifAlias                     =>  '.1.3.6.1.2.1.31.1.1.1.18',
-    ifDescr                     =>  '.1.3.6.1.2.1.2.2.1.2',
-    ifSpeed                     =>  '.1.3.6.1.2.1.2.2.1.5',
-    ifAdminStatus               =>  '.1.3.6.1.2.1.2.2.1.7',
-    ifOperStatus                =>  '.1.3.6.1.2.1.2.2.1.8',
-    ifInOctets                  =>  '.1.3.6.1.2.1.2.2.1.10',
-    ifOutOctets                 =>  '.1.3.6.1.2.1.2.2.1.16',
-    dot1qVlanStaticUntaggedPort =>  '.1.3.6.1.2.1.17.7.1.4.3.1.4',
+    ifNumber                        =>  '.1.3.6.1.2.1.2.1.0',
+    vmMembershipEntry               =>  '.1.3.6.1.4.1.9.9.68.1.2.2.1.2',    #CISCO
+    vtpVlanName                     =>  '.1.3.6.1.4.1.9.9.46.1.3.1.1.4.1',  #CISCO
+    dot1dBasePortIfIndex            =>  '.1.3.6.1.2.1.17.1.4.1.2',          #ENT
+    dot1qVlanStaticName             =>  '.1.3.6.1.2.1.17.7.1.4.3.1.1',      #ENT
+    dot1qPvid                       =>  '.1.3.6.1.2.1.17.7.1.4.5.1.1',      #ENT
+    ifIndex                         =>  '.1.3.6.1.2.1.2.2.1.1',
+    ifName                          =>  '.1.3.6.1.2.1.31.1.1.1.1',
+    ifAlias                         =>  '.1.3.6.1.2.1.31.1.1.1.18',
+    ifDescr                         =>  '.1.3.6.1.2.1.2.2.1.2',
+    ifSpeed                         =>  '.1.3.6.1.2.1.2.2.1.5',
+    ifAdminStatus                   =>  '.1.3.6.1.2.1.2.2.1.7',
+    ifOperStatus                    =>  '.1.3.6.1.2.1.2.2.1.8',
+    ifInOctets                      =>  '.1.3.6.1.2.1.2.2.1.10',
+    ifOutOctets                     =>  '.1.3.6.1.2.1.2.2.1.16',
+    dot1qVlanStaticEgressPorts      =>  '.1.3.6.1.2.1.17.7.1.4.3.1.2',
+    dot1qVlanForbiddenEgressPorts   =>  '.1.3.6.1.2.1.17.7.1.4.3.1.3',
+    dot1qVlanStaticUntaggedPorts    =>  '.1.3.6.1.2.1.17.7.1.4.3.1.4',
+    sysInfo                         =>  '.1.3.6.1.2.1.1'
 );
 
-sub GetSnmp() {
-    my ($machine, $community, @oids) = @_;
+my $table = '<TABLE style="font-size:9px; text-align: center; border-style:solid; border-width:1px;" border="0" cellspacing="3" SIZE=6>';
+my %sinfo=();
+my %vlans=();
+
+sub SnmpSession() {
+    my ($machine, $community) = @_;
 
     $machine   = 'localhost' unless $machine;
     $community = 'public'    unless $community;
@@ -41,27 +48,59 @@ sub GetSnmp() {
         RemotePort  => 161,
         UseNumeric  => 1,
     );
+    return $session;
+}
 
+sub get() {
+    my ($machine, $community, $oid) = @_;
+    my $session = &SnmpSession($machine, $community);
+    if ( $session->{ErrorNum} ) {
+        print "<$@> Error ".$session->{ErrorNum}." \"".$session->{ErrorStr}."\n en ".$session->{ErrorInd}."\n";
+    }
+    return $session->get($oid);
+}
+
+sub bulk() {
+    my ($machine, $community, @oids) = @_;
+    my $session = &SnmpSession($machine, $community);
     my @VarBinds =();
     foreach (@oids) {
         push @VarBinds, new SNMP::Varbind([$_]);
     }
-
     my $VarList = new SNMP::VarList( @VarBinds );
-    my @result = $session->bulkwalk( 0, 75, $VarList );
+    my $ifnum = &get($machine, $community,$oids{'ifNumber'});
+    my @result = $session->bulkwalk( 0, $ifnum, $VarList );
 
     if ( $session->{ErrorNum} ) {
-        print "Error ".$session->{ErrorNum}." \"".$session->{ErrorStr}."\n en ".$session->{ErrorInd}."\n";
+        print "<$@> Error ".$session->{ErrorNum}." \"".$session->{ErrorStr}."\n en ".$session->{ErrorInd}."\n";
     }
 
-    my %vlans=();
+    return @result;
+}
+
+
+sub parse() {
+    my @result = @_;
+
     for my $x ($result[11]){
         for my $y (@$x){
-            $vlans{@$y[1]} = @$y[2];
+            $vlans{@$y[1]} = {
+                        name        => @$y[2],
+                        pvid        => [],
+                        egress      => [],
+                        forbegress  => [],
+                        untagged    => []
+            };
         }
     }
 
-    my %ifaces = {};
+   for my $x ($result[12]){
+        for my $y (@$x){
+            $sinfo{@$y[0]} = @$y[2];
+        }
+    }
+
+    my %ifaces = ();
     for (0..$#{$result[$_]}) {
         $ifaces{$result[0][$_][2]} = {
             index       => $result[0][$_][2],
@@ -75,19 +114,61 @@ sub GetSnmp() {
             out         => &convert_bytes($result[8][$_][2], 3),
             pvid        => $result[9][$_][2],
             vlanport    => $result[10][$_][2],
-            vlaname     => $vlans{$result[9][$_][2]}
-        }; 
+            vlaname     => $vlans{$result[9][$_][2]}{'name'},
+            egress      => [],
+            untagged    => []
+        };
     }
-    #print Dumper %ifaces;
+
+
+    for my $x ($result[9]){
+        for my $y (@$x){
+                push ( @{ $vlans{@$y[2]}{'pvid'} }, $ifaces{@$y[1]}{'name'} ) if @$y[1];
+        }
+    }
+
+    for my $x ($result[13]){
+        for my $y (@$x){
+            my $pno = 0;
+            foreach (split(//,unpack("B*", @$y[2]))) {
+                $pno++; next unless $_ eq 1;
+                push ( @{ $vlans{@$y[1]}{'egress'} }, $ifaces{$pno}{'name'} ) if $ifaces{$pno}{'name'};
+                push ( @{ $ifaces{$pno}{'egress'} }, @$y[1] );
+            }
+        }
+    }
+
+    for my $x ($result[14]){
+        for my $y (@$x){
+            my $pno = 0;
+            foreach (split(//,unpack("B*", @$y[2]))) {
+                $pno++; next unless $_ eq 1;
+                push ( @{ $vlans{@$y[1]}{'forbegress'} }, $ifaces{$pno}{'name'} ) if $ifaces{$pno}{'name'};
+                push ( @{ $ifaces{$pno}{'forbegress'} }, @$y[1] );
+            }
+        }
+    }
+
+    for my $x ($result[15]){
+        for my $y (@$x){
+            my $pno = 0;
+            foreach (split(//,unpack("B*", @$y[2]))) {
+                $pno++; next unless $_ eq 1;
+                push ( @{ $vlans{@$y[1]}{'untagged'} }, $ifaces{$pno}{'name'} ) if $ifaces{$pno}{'name'};
+                push ( @{ $ifaces{$pno}{'untagged'} }, @$y[1] );
+            }
+        }
+    }
+
     return %ifaces;
 }
 
 sub on_off {
-    return "On" if  $_[0] == 1 or return "Off"; 
+    return "On" if  $_[0] == 1 or return "Off";
 }
 
 sub convert_bytes ($$){
-     my ($bytes, $dec) = @_;    
+     my ($bytes, $dec) = @_;
      foreach my $posfix (qw(bytes Kb Mb Gb Tb Pb Eb Zb Yb)) {
              return sprintf("\%.${dec}f \%s", $bytes, $posfix) if $bytes < 1024;
              $bytes = $bytes / 1024;
@@ -106,11 +187,11 @@ sub TrueSort() { # http://www.perlmonks.org/?node_id=483462
         } 0..$#list
     ];
 }
- 
+
 sub RDigits() { #Devuelve los digitos a la derecha del string
     return $& if "$_[0]" =~ /\d+$/g;
 }
- 
+
 sub AgrArr() { #Agrupa los puertos de un array de un chui
     my @out=();
     my $cache=undef;
@@ -141,74 +222,150 @@ sub getValues {
 }
 
 my %hash=();
-my (@adminports,@operports)=();
-my %FORM=&getValues;
 
-sub tmp() {
-	%hash = &GetSnmp ( $ARGV[0], 'uocpublic',
-	                            $oids{'ifIndex'},
-	                            $oids{'ifName'},
-	                            $oids{'ifAlias'},
-	                            $oids{'ifDescr'},
-	                            $oids{'ifSpeed'},
-	                            $oids{'ifAdminStatus'},
-	                            $oids{'ifOperStatus'},
-	                            $oids{'ifInOctets'},
-	                            $oids{'ifOutOctets'},
-	                            $oids{'dot1qPvid'},
-	                            $oids{'dot1dBasePortIfIndex'},
-	                            $oids{'dot1qVlanStaticName'},
-	#                            $oids{'dot1qVlanStaticUntaggedPorts'}
-	);
-	
-	foreach my $key (keys %hash) {
-	    push (@adminports, $hash{$key}{'name'}) if $hash{$key}{'admin'} eq "Off";
-	    push (@operports, $hash{$key}{'name'}) if $hash{$key}{'oper'} eq "Off";
-	}
-	
-	#print join (", ", &AgrArr(@adminports)); 
-	#print "\n";
-	#print join (", ", &AgrArr(@operports)); 
+sub enterasys() {
+    %hash = &parse( &bulk ( $ARGV[0], 'uocpublic',
+                                $oids{'ifIndex'},
+                                $oids{'ifName'},
+                                $oids{'ifAlias'},
+                                $oids{'ifDescr'},
+                                $oids{'ifSpeed'},
+                                $oids{'ifAdminStatus'},
+                                $oids{'ifOperStatus'},
+                                $oids{'ifInOctets'},
+                                $oids{'ifOutOctets'},
+                                $oids{'dot1qPvid'},
+                                $oids{'dot1dBasePortIfIndex'},
+                                $oids{'dot1qVlanStaticName'},
+                                $oids{'sysInfo'},
+                                $oids{'dot1qVlanStaticEgressPorts'},
+                                $oids{'dot1qVlanForbiddenEgressPorts'},
+                                $oids{'dot1qVlanStaticUntaggedPorts'}
+    )   );
 }
 
-
+sub cisco() {
+    %hash = &parse( &bulk ( $ARGV[0], 'uocpublic',
+                                $oids{'ifIndex'},
+                                $oids{'ifName'},
+                                $oids{'ifAlias'},
+                                $oids{'ifDescr'},
+                                $oids{'ifSpeed'},
+                                $oids{'ifAdminStatus'},
+                                $oids{'ifOperStatus'},
+                                $oids{'ifInOctets'},
+                                $oids{'ifOutOctets'},
+                                $oids{'vmMembershipEntry'},
+                                $oids{'dot1dBasePortIfIndex'},
+                                $oids{'vtpVlanName'},
+                                $oids{'sysInfo'},
+                                $oids{'dot1qVlanStaticEgressPorts'},
+                                $oids{'dot1qVlanForbiddenEgressPorts'},
+                                $oids{'dot1qVlanStaticUntaggedPorts'}
+    )   );
+}
 sub header() {
+print "Content-Type: text/html\n\n";
 print <<EOF;
-<HTML><BODY>
-<B>Test:</B><BR/>
+<HTML>
+    <HEAD>
+        <META http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <TITLE>OAL!</TITLE>
+    </HEAD>
+<BODY>
+<B>Test:</B><BR>
 <FORM action="$ENV{'SCRIPT_NAME'}" method="POST">
-    <table border="0">
-        <tr><td align="right">Host:</td><td><input type="text" name="input" value=""/></td><td><input type="submit" value="Submit"/></td></tr>
+    <TABLE border="0">
+        <TR><TD align="right">Host:</TD><TD><INPUT type="text" name="input" value=""/></TD><TD><INPUT type="submit" value="Submit"/></TD></TR>
     </TABLE>
 </FORM>
 EOF
 }
 
-sub ports() {
-    print '<TABLE style="font-size:9px; text-align: center; border-style:solid; border-width:1px; white-space:nowrap" border="0" cellspacing="3" SIZE=6><TR bgcolor=#AAAAAA><TD>Index</TD><TD>Name</TD><TD>Alias</TD><TD>Speed</TD><TD>Admin</TD><TD>Oper<TD>In</TD><TD>Out</TD><TD>Pvid</TD><TD>Vlan Name</TD><TD>Description</TD></TR>';
-    foreach my $key (sort { $a <=> $b } keys %hash) {
-        next if not exists $hash{$key}{'index'} or $hash{$key}{'pvid'} < 1;
-        my $bgc = '#DDDDDD'; 
-        $bgc = '#B3D98C' if $hash{$key}{'oper'} eq "Off" ;
-        $bgc = '#8CB3D9' if $hash{$key}{'admin'} eq "Off"; 
-        $bgc = '#D9B38C' if $hash{$key}{'admin'} eq "Off" and $hash{$key}{'oper'} eq "Off";
-    #    print "$hash{$key}{'index'}\t$hash{$key}{'name'}\t$hash{$key}{'alias'}\t$hash{$key}{'speed'}\t$hash{$key}{'admin'}\t$hash{$key}{'oper'}\t$hash{$key}{'in'}\t$hash{$key}{'out'}\t$hash{$key}{'pvid'}\t$hash{$key}{'vlanport'}\t$hash{$key}{'vlanname'}\n"; 
-    print "\t<TR bgcolor=$bgc><TD bgcolor=#AAAAAA>$hash{$key}{'index'}</TD><TD>$hash{$key}{'name'}</TD><TD>$hash{$key}{'alias'}</TD><TD>$hash{$key}{'speed'}</TD><TD>$hash{$key}{'admin'}</TD><TD>$hash{$key}{'oper'}</TD><TD>$hash{$key}{'in'}</TD><TD>$hash{$key}{'out'}</TD><TD>$hash{$key}{'pvid'}</TD><TD>$hash{$key}{'vlaname'}</TD><TD>$hash{$key}{'descr'}</TD></TR>\n";
+sub info() {
+    print "<B>Info:</B><BR>\n";
+    print "$table<TR bgcolor=#AAAAAA><TD>Index</TD><TD>Name</TD></TR>";
+    foreach my $key (sort keys %sinfo) {
+        print "\t<TR bgcolor=#DDDDDD><TD bgcolor=#AAAAAA>$key</TD>".&td($sinfo{$key})."</TR>\n";
     }
     print "</TABLE>\n";
-}    
+}
+
+sub dbg() {
+   print "<pre>";
+   print Dumper $_[0];
+   print "</pre>";
+}
+
+sub totals() {
+    my (@adminports,@operports)=();
+    print "<B>Ports:</B><BR/>";
+    print "$table<TR bgcolor=#AAAAAA>".&td('Range').&td('Ports').&td('Total')."</TR>";
+    foreach my $key (keys %hash) {
+        push (@adminports, $hash{$key}{'name'}) if $hash{$key}{'admin'} eq "Off";
+        push (@operports, $hash{$key}{'name'}) if $hash{$key}{'oper'} eq "Off";
+    }
+    print "\t<TR bgcolor=#DDDDDD><TD bgcolor=#AAAAAA>Admin Off</TD>".&td(join (", ", &AgrArr(@adminports))).&td(($#adminports+1))."</TR>\n";
+    print "\t<TR bgcolor=#DDDDDD><TD bgcolor=#AAAAAA>Oper Off</TD>".&td(join (", ", &AgrArr(@operports))).&td(($#operports+1))."</TR>\n";
+    print "</TABLE>\n";
+}
+
+sub vlans() {
+    print "<B>Vlans:</B><BR/>";
+    print "$table<TR bgcolor=#AAAAAA><TD>Nombre</TD><TD>Pvid</TD><TD>Tipo</TD><TD>Puertos</TD><TD>Total</TD></TR>";
+    foreach my $key (sort { $a <=> $b } keys %vlans) {
+        next unless $key;
+        print "\t<TR bgcolor=#DDDDDD><TD rowspan=\"4\" bgcolor=#AAAAAA>$vlans{$key}{'name'}</TD><TD rowspan=\"4\" >$key</TD><TD bgcolor=#BBBBBB>Pvid</TD>".&td(join (", ", &AgrArr( @{ $vlans{$key}{'pvid'}} ))).&td(($#{ $vlans{$key}{'pvid'}}+1))."</TR>\n";
+        print "\t<TR bgcolor=#DDDDDD><TD bgcolor=#DDDDDD>Egress</TD>".&td(join (", ", &AgrArr( @{ $vlans{$key}{'egress'}} ))).&td(($#{ $vlans{$key}{'egress'}}+1))."</TR>\n";
+        print "\t<TR bgcolor=#DDDDDD><TD bgcolor=#BBBBBB>Forbidden Egress</TD>".&td(join (", ", &AgrArr( @{ $vlans{$key}{'forbegress'}} ))).&td(($#{ $vlans{$key}{'forbegress'}}+1))."</TR>\n";
+        print "\t<TR bgcolor=#DDDDDD><TD bgcolor=#DDDDDD>Untagged</TD>".&td(join (", ", &AgrArr( @{ $vlans{$key}{'untagged'}} ))).&td(($#{ $vlans{$key}{'untagged'}}+1))."</TR>\n";
+    }
+    print "</TABLE>\n";
+}
+
+sub ports() {
+    print '<B>Ports:</B><BR/>';
+    print "$table<TR bgcolor=#AAAAAA><TD>Index</TD><TD>Name</TD><TD>Alias</TD><TD>Speed</TD><TD>Admin</TD><TD>Oper</TD><TD>In</TD><TD>Out</TD><TD>Pvid</TD><TD>Egress</TD><TD>Untagged</TD><TD>Vlan Name</TD><TD>Description</TD></TR>";
+    foreach my $key (sort { $a <=> $b } keys %hash) {
+        next if not $hash{$key}{'index'} or $hash{$key}{'pvid'} < 1;
+        my $bgc = '#DDDDDD';
+        $bgc = '#B3D98C' if $hash{$key}{'oper'} eq "Off";
+        $bgc = '#8CB3D9' if $hash{$key}{'admin'} eq "Off";
+        $bgc = '#D9B38C' if $hash{$key}{'admin'} eq "Off" and $hash{$key}{'oper'} eq "Off";
+    print "\t<TR bgcolor=$bgc><TD bgcolor=#AAAAAA>$hash{$key}{'index'}</TD>".&td($hash{$key}{'name'}).&td($hash{$key}{'alias'}).&td($hash{$key}{'speed'}).&td($hash{$key}{'admin'}).&td($hash{$key}{'oper'}).&td($hash{$key}{'in'}).&td($hash{$key}{'out'}).&td($hash{$key}{'pvid'}).&td(@{$hash{$key}{'egress'}}).&td(@{$hash{$key}{'untagged'}}).&td($hash{$key}{'vlaname'}).&td($hash{$key}{'descr'})."</TR>\n";
+    }
+    print "</TABLE>\n";
+}
+
+sub td() {
+    return "<TD>@_</TD>" if $_[0] or return "<TD bgcolor=#B9B974><I>Vacío</I></TD>";
+}
+
 
 sub end() {
 #    foreach my $key (keys %ENV) {
 #        print "$key -> $ENV{$key}<br>\n";
 #    }
-    print "</HTML></BODY>";
+    print "</BODY></HTML>";
 }
 
+
+my %FORM=&getValues;
 &header;
 if ($FORM{'input'}) {
-	$ARGV[0] = $FORM{'input'};
-	&tmp;
+    $ARGV[0] = $FORM{'input'};
+    my $sysObjectID = &get($FORM{'input'},'uocpublic','.1.3.6.1.2.1.1.2.0');
+    if ($sysObjectID =~ /\.1\.3\.6\.1\.4\.1\.5624/) {
+        &enterasys;
+    } elsif ($sysObjectID =~ /\.1\.3\.6\.1\.4\.1\.9/) {
+        &cisco;
+    } else {
+        print "<B>($FORM{'input'}) '$sysObjectID' No está soportado.</B>";
+        die;
+    }
+    &info;
+    &totals;
+    &vlans;
     &ports;
 }
 &end;
