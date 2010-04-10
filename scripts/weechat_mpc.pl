3f54a197a80238d694c32d39a1e768317abf0214
Oscar Prieto <oscar@babau.(none)>
2010-04-10 23:33:45 +0200

    update

--- scripts/weechat_mpc.pl
+++ scripts/weechat_mpc.pl
@@ -3,12 +3,13 @@ weechat::hook_command("mpc", "", "", "", "", "info", "");
 
 sub info {
     my ($data, $buffer, $args) = @_;
-    my ($name, $artist, $album, $title, $track, $time, $file, $stats, $footer) = split("\n", `mpc -f "%name%\n%artist%\n%album%\n%title%\n%track%\n%time%\n%file%"`);
-    #$artist =~ tr/\000-\177//cd; #Only non-extended ASCII
-    $artist =~ s/\s\/\s[^,]+//g if length($artist) > 60;
-    $artist = 'V.A.'if length($artist) > 60;
-    $stats =~ s/\s+/ /g;
-    if ($artist and $title and $album) {
+    $args = "127.0.0.1" unless $args;
+    my ($name, $artist, $album, $title, $track, $time, $file, $stats, $footer) = split("\n", `mpc -f "%name%\n%artist%\n%album%\n%title%\n%track%\n%time%\n%file%" -h $args 2>/dev/null`);
+    if ($artist and $title and $album and $stats) {
+        #$artist =~ tr/\000-\177//cd; #Only non-extended ASCII
+        $artist =~ s/\s\/\s[^,]+//g if length($artist) > 60;
+        $artist = 'V.A.'if length($artist) > 60;
+        $stats =~ s/\s+/ /g;
         weechat::command($buffer, "$stats Track $track: $artist - $title, From The Album \'$album\'");
     }
     return weechat::WEECHAT_RC_OK;
